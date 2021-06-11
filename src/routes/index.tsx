730c277ef1d0b717faf4170ea200480dec87e1ce
Ryan Lucas <ryanlucasvc@gmail.com>
2021-06-11 15:57:58 -0300

    feat(navigator): new screens animations.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {
+  CardStyleInterpolators,
+  createStackNavigator,
+  StackNavigationOptions,
+} from '@react-navigation/stack';
 
 import {ListsProvider} from '@contexts/ListsContext';
 import {ThemeProvider} from '@contexts/ThemeContext';
@@ -12,16 +16,39 @@ import Home from '@pages/Home';
 
 const Stack = createStackNavigator();
 
+const horizontalIOS = {
+  cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+} as StackNavigationOptions;
+const verticalIOS = {
+  cardStyleInterpolator: CardStyleInterpolators.forVerticalIOS,
+} as StackNavigationOptions;
+
 export default () => {
   return (
     <NavigationContainer>
       <ThemeProvider>
         <ListsProvider>
           <Stack.Navigator headerMode="none" initialRouteName="Home">
-            <Stack.Screen name="Home" component={Home} />
-            <Stack.Screen name="CreateList" component={CreateList} />
-            <Stack.Screen name="EditList" component={EditList} />
-            <Stack.Screen name="CreateItem" component={CreateItem} />
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={horizontalIOS}
+            />
+            <Stack.Screen
+              name="CreateList"
+              component={CreateList}
+              options={verticalIOS}
+            />
+            <Stack.Screen
+              name="EditList"
+              component={EditList}
+              options={horizontalIOS}
+            />
+            <Stack.Screen
+              name="CreateItem"
+              component={CreateItem}
+              options={verticalIOS}
+            />
           </Stack.Navigator>
         </ListsProvider>
       </ThemeProvider>
